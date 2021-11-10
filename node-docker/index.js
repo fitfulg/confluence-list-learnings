2795f1676911abc919b54d6f7c9e4c0ff12127e6
ercardona <ercardona@edrans.com>
2021-11-10 11:07:50 +0100

    /api/v1/posts issue

--- node-docker/index.js
+++ node-docker/index.js
@@ -7,6 +7,8 @@ const {
   MONGO_PORT,
 } = require("./config/config");
 
+const postRouter = require("./routes/postRoutes");
+
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -23,10 +25,14 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
-app.get("/", (req, res) => {
-  res.send("<h2>Hi there!!!!!!</H2>");
+app.use(express.json());
+
+app.get("/api/v1/", (req, res) => {
+  res.send("<h2>Hi  There</h2>");
+  console.log("yeah it ran");
 });
 
+app.use("/api/v1/posts", postRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
